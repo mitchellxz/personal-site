de4094796cc39a08190c86d560f21d1544c35aed
Mitchell <mitchell.ramos1028@gmail.com>
2024-05-19 01:22:31 -0400

    test

--- personal-site/src/App.jsx
+++ personal-site/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       >
         <h1>Hello, World!</h1>
         <h1>Hello, World!</h1>
-        <h1>Hello, World!</h1>
+        <h1>Hello, Everyone!</h1>
       </Container>
       <Container
         className="featuredProjects"
