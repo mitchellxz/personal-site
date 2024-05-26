7856376812a5e2f2e212693a5ee33ff997e194af
Mitchell <mitchell.ramos1028@gmail.com>
2024-05-26 19:41:19 -0400

    add content

--- personal-site/src/pages/AboutmePage.jsx
+++ personal-site/src/pages/AboutmePage.jsx
@@ -1,5 +1,6 @@
 import Container from "../layout/Container";
 import Navbar from "../components/Navbar";
+import Card from "../components/Card";
 
 function AboutmePage() {
   return (
@@ -16,15 +17,56 @@ function AboutmePage() {
       </Container>
       <Container className="introduction">
         <h1>Introduction</h1>
-        introduintroductionintroductionintroductionintroductionintroductionintroductionintroductionintroductionintroductionintroductionction
+        <p>
+          Hi, I&apos;m Mitchell, a graduate based in Delaware. I graduated from
+          University of Delaware with a degree in Computer Science. I am skilled
+          in the following languages:
+          <ul className="languageList">
+            <li>Java</li>
+            <li>Python</li>
+            <li>SQL</li>
+            <li>HTML/CSS</li>
+            <li>R</li>
+            <li>JavaScript</li>
+            <li>C/C++</li>
+            <li>TypeScript</li>
+          </ul>
+          I have also worked with the following developer tools & frameworks:
+          <ul className="tools">
+            <li>IntelliJ</li>
+            <li>Visual Studio</li>
+            <li>VS Code</li>
+            <li>SQL Developer</li>
+            <li>Git</li>
+            <li>Postman</li>
+            <li>PostgreSQL</li>
+            <li>Django</li>
+            <li>Bootstrap</li>
+          </ul>
+        </p>
       </Container>
       <Container className="hobbies">
         <h1>Hobbies</h1>
-        hobbieshobbieshobbieshobbieshobbieshobbieshobbieshobbieshobbieshobbies
+        <p>
+          In my free time I enjoy traveling and exploring new places. Here are
+          some places I have recently visted:
+        </p>
+        <div className="travelCards">
+          <Card
+            image="../src/assets/korea.jpeg"
+            cardContent={"South Korea"}
+          ></Card>
+          <Card image="../src/assets/japan.jpeg" cardContent={"Japan"}></Card>
+          <Card image="../src/assets/miami.jpeg" cardContent={"Miami"}></Card>
+        </div>
       </Container>
       <Container className="contact">
         <h1>Contact</h1>
-        contactcontactcontactcontactcontact
+        <p>
+          Feel free to reach out to me at <a href="mailto:[email]">Email</a> or
+          connect with me on{" "}
+          <a href="https://www.linkedin.com/in/mitchellrn/">LinkedIn</a>
+        </p>
       </Container>
     </>
   );
--- personal-site/src/pages/ProjectsPage.jsx
+++ personal-site/src/pages/ProjectsPage.jsx
@@ -26,22 +26,18 @@ function ProjectsPage() {
               image="../src/assets/preview1.png"
               cardContent="Embark on a thrilling journey in Boolean Bonanza, a brain-teasing puzzle game inspired by classics like Yoshi's Cookie and Candy Crush. Swap tiles strategically to create true boolean expressions, sharpening your logical reasoning skills with each level. Progress through increasingly challenging puzzles set in a vibrant Mario-themed world accompanied by playful music, adding to the immersive experience. With retro aesthetics and engaging gameplay, Boolean Bonanza offers a delightful challenge for students of logic, programming, and puzzle enthusiasts alike."
             />
-            <p>Description</p>
           </div>
           <div className="project-item">
             <h2>Festijam</h2>
             <Card image="../src/assets/preview2.png" cardContent="test" />
-            <p>Description</p>
           </div>
           <div className="project-item">
             <h2>Project 3</h2>
             <Card image="../src/assets/preview1.png" cardContent="test" />
-            <p>Description</p>
           </div>
           <div className="project-item">
             <h2>Project 4</h2>
             <Card image="../src/assets/preview1.png" cardContent="test" />
-            <p>Description</p>
           </div>
         </Container>
       </div>
